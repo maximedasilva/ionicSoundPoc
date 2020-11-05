f57c128caebdba913d697f893ddcf4be93e4c107
maximedasilva <maximedasilva40@gmail.com>
2020-11-05 17:51:24 +0100

    WIP soundPOC

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Media } from '@ionic-native/media/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,13 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private media: Media) {}
 
+  play_The_track: string = "/android_asset/public/assets/sound.mp3"; //note this specific url format is used in android only
+  playSound() {
+    const curr_playing_file = this.media.create(this.play_The_track);
+    // on occassions, the plugin only gives duration of the file if the file is played
+    // at least once
+    curr_playing_file.play();
+  }
 }
